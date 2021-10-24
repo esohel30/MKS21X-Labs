74e592b1f29aeca2519f383ad97c6c62c7f6730f
esohel30 <esohel30@stuy.edu>
2021-10-23 22:24:59 -0400

    fourth commit

--- homework22/Demo.java
+++ homework22/Demo.java
@@ -1,4 +1,5 @@
 public class Demo{
+
   public static double distance(Point p1, Point p2){
     double x1 = p1.getX();
     double y1 = p1.getY();
@@ -10,10 +11,19 @@ public class Demo{
   public static void main(String[]args){
     Point P1 = new Point(1,1);
     Point P2 = new Point(2,2);
+    Point P3 = new Point(4,4);
+    Point P4 = new Point(-2,10202);
 
     System.out.println(distance(P1,P2)); //call1
     System.out.println(Point.distance(P1,P2)); //call 2
     System.out.println(P1.distanceTo(P2)); //call 3
+
+    System.out.println(distance(P1,P4));
+//  should return 10201.00
+    System.out.println(Point.distance(P3,P4));
+//  should return 10198.001
+    System.out.println(P2.distanceTo(P2));
+//  should return 0
     //This one is IMPLIED strongly. You cannot tell by the calling style alone,
     //but you can make an educated guess.
   }
