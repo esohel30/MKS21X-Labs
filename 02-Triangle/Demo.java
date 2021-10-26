d7fa3e21c9a3c17ea117d31c8b8e05304e59dc80
esohel30 <esohel30@stuy.edu>
2021-10-25 20:52:53 -0400

    beautified java code

--- 02-Triangle/Demo.java
+++ 02-Triangle/Demo.java
@@ -1,31 +1,28 @@
+public class Demo {
 
-public class Demo{
+    public static double distance(Point p1, Point p2) {
+        double x1 = p1.getX();
+        double y1 = p1.getY();
+        double x2 = p2.getX();
+        double y2 = p2.getY();
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y1, 2));
+    }
 
-  public static double distance(Point p1, Point p2){
-    double x1 = p1.getX();
-    double y1 = p1.getY();
-    double x2 = p2.getX();
-    double y2 = p2.getY();
-    return Math.sqrt(Math.pow(x1 -x2,2) + Math.pow(y2-y1,2));
-  }
+    public static void main(String[] args) {
+        Point P1 = new Point(1, 1);
+        Point P2 = new Point(2, 2);
+        Point P3 = new Point(4, 4);
+        Point P4 = new Point(-2, 10202);
 
-  public static void main(String[]args){
-    Point P1 = new Point(1,1);
-    Point P2 = new Point(2,2);
-    Point P3 = new Point(4,4);
-    Point P4 = new Point(-2,10202);
+        System.out.println(distance(P1, P2)); //call1
+        System.out.println(Point.distance(P1, P2)); //call 2
+        System.out.println(P1.distanceTo(P2)); //call 3
+        // test cases
+        System.out.println(distance(P1, P4));
+        //  should return 10201.00
+        System.out.println(Point.distance(P3, P4));
+        //  should return 10198.001
+        System.out.println(P2.distanceTo(P2));
+        //  should return 0
 
-    System.out.println(distance(P1,P2)); //call1
-    System.out.println(Point.distance(P1,P2)); //call 2
-    System.out.println(P1.distanceTo(P2)); //call 3
-// test cases
-    System.out.println(distance(P1,P4));
-//  should return 10201.00
-    System.out.println(Point.distance(P3,P4));
-//  should return 10198.001
-    System.out.println(P2.distanceTo(P2));
-//  should return 0
-
-  }
-
- }
+    }
--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -1,41 +1,42 @@
-public class Traingle{
-  private point v1, v2, v3;
+public class Traingle {
+    private point v1, v2, v3;
 
-  public Triangle(point p1, point p2, point p3){
-    v1 = p1;
-    v2 = p2;
-    v3 = p3;
-  }
+    public Triangle(point p1, point p2, point p3) {
+        v1 = p1;
+        v2 = p2;
+        v3 = p3;
+    }
 
-  public double getPerimeter(){
-    return Point.distance(v3, v1) + Point.distance(v2, v3) + Point.distance(v1, v2);
-  }
+    public double getPerimeter() {
+        return Point.distance(v3, v1) + Point.distance(v2, v3) + Point.distance(v1, v2);
+    }
 
-  public point getVertex(int val){
-    if(val ==1){
-      return v1;
+    public point getVertex(int val) {
+        if (val == 1) {
+            return v1;
+        }
+        if (val == 2) {
+            return v2;
+        }
+        if (val == 3) {
+            return v3;
+        }
     }
-    if (val ==2){
-      return v2;
+    public void setVertex(int val, Point constant) {
+        if (val = 1) {
+            v1 = constant;
+        }
+        if (val == 2) {
+            v2 = constant;
+        }
+        if (val == 3) {
+            v2 = constant;
+        }
     }
-    if(val ==3){
-      return v3;
-    }
-  }
-  public void setVertex(int val, Point constant){
-   if (val =1){
-    v1 =constant;
-    }
-   if (val ==2){
-    v2 = constant;
-   }
-   if (val ==3){
-    v2 = constant;
-  }
-}
 
-public String toString(){
-  return "[" +
-  v1 + "," +  v2 + "," +v3
-  +"]"}
+    public String toString() {
+        return "[" +
+            v1 + "," + v2 + "," + v3 +
+            "]"
+    }
 }
