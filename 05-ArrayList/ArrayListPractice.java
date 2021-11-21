db2bf6f1a1bfbfce85a1851fbaa73c91f5b85923
esohel30 <esohel30@stuy.edu>
2021-11-20 20:56:31 -0500

    finished all fuctions. now it is time to add test randomized test cases

--- 05-ArrayList/ArrayListPractice.java
+++ 05-ArrayList/ArrayListPractice.java
@@ -4,40 +4,83 @@ public class ArrayListPractice {
 public static ArrayList<String> replaceEmpty( ArrayList<String> data) {
   //return an new ArrayList that has all of the values of the original ArrayList except
   //that all empty strings are replaced with the word "Empty".
-  for(int i=0; i <data.size(); i++) {
-    if(data.get(i).equals("")) {
-      data.set(i, "empty");
+  ArrayList<String>replaced = new ArrayList<String>();
+  for(int i =0; i < data.size(); i++) {
+    replaced.add(data.get(i));
+  }
+  for(int i=0; i< replaced.size(); i++) {
+    if(replaced.get(i).equals("")) {
+      replaced.set(i, "empty");
     }
   }
-return data;
+  return replaced;
 }
-
 
 public static ArrayList<String> makeReversedList( ArrayList<String> data){
   //return a new ArrayList that is in the reversed order of the parameter.
-  ArrayList<String>reverse = new ArrayList<String>();
+  ArrayList<String>reversed = new ArrayList<String>();
   for(int i = data.size()-1; i > -1; i --){
-    reverse.add(data.get(i));
+    reversed.add(data.get(i));
     }
-  return reverse;
+  return reversed;
 }
 
 
-//public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
   //return a new ArrayList that has all values of a and b in alternating order that is:
   //a[0], b[0], a[1], b[1]...
   //when one list is longer than the other, just append the remaining values to the end.
-//  }
+  ArrayList<String>mixed = new ArrayList<String>();
+  if(a.size() == b.size()){
+    for(int i=0; i< b.size(); i ++){
+      mixed.add(a.get(i));
+      mixed.add(b.get(i));
+    }
+    }
+  if(a.size() < b.size()){
+    for(int i=0; i< a.size(); i ++){
+      mixed.add(a.get(i));
+      mixed.add(b.get(i));
+    }
+    for(int i = a.size() ; i < b.size(); i ++){
+      mixed.add(b.get(i));
+    }
+  }
+
+  if(a.size() > b.size()){
+    for(int i=0; i< b.size(); i ++){
+      mixed.add(a.get(i));
+      mixed.add(b.get(i));
+    }
+    for(int i = b.size() ; i < a.size(); i ++){
+      mixed.add(a.get(i));
+    }
+  }
+
+    return mixed;
+ }
+
 
   public static void main(String[] args) {
     ArrayList<String>words = new ArrayList<String>();
-    words.add("morning");
-    words.add("evening");
-    words.add("night");
-    words.add("");
+    ArrayList<String>birds = new ArrayList<String>();
+
+    words.add("I");
+    words.add("love");
+    words.add("cake");
+    birds.add("really");
+    birds.add("good");
+    birds.add("lol");
+    birds.add("lol");
+
+
+
     System.out.println(words);
-    replaceEmpty(words);
+    System.out.println(replaceEmpty(words));
     System.out.println(words);
+    System.out.println(makeReversedList(words));
+    System.out.println(mixLists(birds, words));
+
 
   }
 }
