f56c2244188cd1f5bed5d56047027329ba810dd4
esohel30 <esohel30@stuy.edu>
2021-11-20 19:51:31 -0500

    worked on and tested first fuction remplace empty

---
+++ 05-ArrayList/ArrayListPractice.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class ArrayListPractice {
+
+public static ArrayList<String> replaceEmpty( ArrayList<String> data) {
+  //return an new ArrayList that has all of the values of the original ArrayList except
+  //that all empty strings are replaced with the word "Empty".
+  for(int i=0; i <data.size(); i++) {
+    if(data.get(i).equals("")) {
+      data.set(i, "empty");
+    }
+  }
+return data;
+}
+
+
+
+
+
+
+
+
+
+public static ArrayList<String> makeReversedList( ArrayList<String> data){
+  //return a new ArrayList that is in the reversed order of the parameter.
+  
+}
+
+
+public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //when one list is longer than the other, just append the remaining values to the end.
+
+
+
+//  }
