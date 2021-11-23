d7023bbca5f86806b843ce4b10164fdd4ba0db87
esohel30 <esohel30@stuy.edu>
2021-11-23 18:41:10 -0500

    working on advent wrapping paper problem; set up variables and a potential loop

---
+++ 06-Advent/Wrapping.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Wrapping {
+  public static void main(String[] args) {
+
+
+    try {
+    String fileName = args[0];
+    File file  = new File(fileName);
+    Scanner input = new Scanner(file);
+    int count = 0;
+
+    while(input.hasNextLine()) {
+      String line = input.nextLine();
+      String[]nums = line.split("x");
+
+      int length = Integer.parseInt(nums[0]);
+      int width  = Integer.parseInt(nums[1]);
+      int height = Integer.parseInt(nums[2]);
+      int b1 = length * width;
+      int b2 = length * height;
+      int b3 = width  * height;
+
+    
+      }
+      count += (2 * b1 + 2 * b2 + 2 * b3 + min);
+    }
+    System.out.println(count);
+  }
+
+catch (FileNotFoundException ex) {
+      System.out.println("insert file name properly");
+}
+}
+}
