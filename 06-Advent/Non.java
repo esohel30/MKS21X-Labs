b24601fef0c4a2c4c29d87d393432b1c5d0d10af
esohel30 <esohel30@stuy.edu>
2021-12-01 12:01:55 -0500

    added new methods and tested them for part two

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -40,31 +40,66 @@ public class Non {
       return true;
     }
 
+    public static boolean doublePair(String a) {
+      int len = a.length();
+      for(int i =0; i < len -1; i++){
+        String currentPair = a.substring(i, i+2);
+        String rest = a.substring(i+2, len);
+
+        if(rest.contains(currentPair)){
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public static boolean spaceRep(String a) {
+      int len = a.length();
+      for(int i=0; i< len-2; i++){
+        if(a.substring(i, i+1).equals(a.substring(i+2, i+3))){
+          return true;
+        }
+      }
+      return false;
+    }
+
+
     public static void main(String[] args) {
       try {
-           if(args.length == 0){
-             System.out.println("insert a file name next time");
-             System.exit(1);
-           }
+         if(args.length == 0){
+           System.out.println("insert a file name next time");
+           System.exit(1);
+         }
 
-           File file = new File(args[0]);
-           Scanner input = new Scanner(file);
-           int count = 0;
+         File file = new File(args[0]);
+         Scanner input = new Scanner(file);
+         int method = Integer.parseInt(args[1]);
+         int count = 0;
 
+         if(method == 1){
            while(input.hasNextLine()){
              String currentLine = input.nextLine();
              if(hasVowels(currentLine) && twice(currentLine) && exceptions(currentLine)){
               count ++;
-             }
            }
-
-       System.out.println(count);
-     }
-
-       catch (FileNotFoundException ex) {
-           System.out.println("insert file name properly");
+         }
        }
 
+       if(method == 2){
+         while(input.hasNextLine()){
+           String currentLine = input.nextLine();
+           if(doublePair(currentLine) && spaceRep(currentLine)){
+            count ++;
+       }
+     }
+   }
+
+     System.out.println(count);
+   }
+
+     catch (FileNotFoundException ex) {
+         System.out.println("insert file name properly");
+     }
     }
 
 }
