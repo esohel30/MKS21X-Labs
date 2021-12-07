d922a3ce756dc1236291fb0d1cdeb39e13bb9a0e
esohel30 <esohel30@stuy.edu>
2021-12-07 10:16:03 -0500

    finished my son slection sort and my brain hurts

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -1,59 +1,75 @@
 import java.util.Arrays;
 import java.util.Random;
-  public class Sorts{
-  /**Bubble sort of an int array.
-  *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
-  */
 
-  // sort number one
-  public static void bubbleSort(int[] data){
-    int len = data.length -1;
-    for(int i =0; i < len; i++){
-      if(data[i] < data[i+1] && i != len -1){
-        i++;
-      }
-      if(data[i] > data[i+1]){
-        int current = data[i];
-        data[i] = data[i+1];
-        data[i+1] = current;
-        i = -1;
+public class Sorts {
+    /**Bubble sort of an int array.
+     *Upon completion, the elements of the array will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+
+    // sort number one
+    public static void bubbleSort(int[] data) {
+        int len = data.length - 1;
+        for (int i = 0; i < len; i++) {
+            if (data[i] < data[i + 1] && i != len - 1) {
+                i++;
+            }
+            if (data[i] > data[i + 1]) {
+                int current = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = current;
+                i = -1;
+            }
         }
     }
-  }
-  // sort number two 
-  public static void selectionSort(int[] ary) {
-    int start = 0;
-    int temp  = 0;
-    int min =0;
-    for(int i =0; i < ary.length; i++){
-      for(int j =start; j < ary.length; j++){
-        min = ary[start];
-        if(ary[j] < min){
-          min = ary[j];
-          temp = ary[start];
-          ary[start] = min;
-          ary[j] = temp;
+
+    // sort number two
+    public static void selectionSort(int[] ary) {
+        int start = 0;
+        int temp = 0;
+        int min = 0;
+        for (int i = 0; i < ary.length; i++) {
+            for (int j = start; j < ary.length; j++) {
+                min = ary[start];
+                if (ary[j] < min) {
+                    min = ary[j];
+                    temp = ary[start];
+                    ary[start] = min;
+                    ary[j] = temp;
+                }
+            }
+            start++;
         }
-      }
-      start ++;
     }
+
+    // sort number three
+    public static void insertionSort(int[] data) {
+        int len = data.length;
+        for (int i = 1; i < len; i++) {
+            int index = i - 1;
+            int current = data[i];
+            while ((index >= 0) && (data[index] > current)) {
+                data[index + 1] = data[index];
+                index--;
+            }
+            data[index + 1] = current;
+        }
     }
 
 
-  public static boolean compares(int[] a, int[] b) {
-    int lenA = a.length;
-    int lenB = b.length;
-    if(lenA != lenB){
-      return false;
-    }
+    public static boolean compares(int[] a, int[] b) {
+        int lenA = a.length;
+        int lenB = b.length;
+        if (lenA != lenB) {
+            return false;
+        }
 
-    for(int i =0; i < lenA; i++){
-      if(a[i] != b[i]){
-        return false;
-      }
-    }
-    return true;
+        for (int i = 0; i < lenA; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
   public static void main(String[] args) {
@@ -90,15 +106,15 @@ import java.util.Random;
       Arrays.sort(h1);
       Arrays.sort(i1);
 
-      selectionSort(a);
-      selectionSort(b);
-      selectionSort(c);
-      selectionSort(d);
-      selectionSort(e);
-      selectionSort(f);
-      selectionSort(g);
-      selectionSort(h);
-      selectionSort(i);
+      insertionSort(a);
+      insertionSort(b);
+      insertionSort(c);
+      insertionSort(d);
+      insertionSort(e);
+      insertionSort(f);
+      insertionSort(g);
+      insertionSort(h);
+      insertionSort(i);
 
     System.out.println(compares(a,a1));
     System.out.println(compares(b,b1));
