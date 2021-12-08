148fcf75e83a3482c5ed09f0cdabeb82e1c98626
esohel30 <esohel30@stuy.edu>
2021-12-07 22:40:50 -0500

    added finishing touches to sort and checked timing of all three sorts

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -2,23 +2,24 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Sorts {
-    /**Bubble sort of an int array.
-     *Upon completion, the elements of the array will be in increasing order.
-     *@param data  the elements to be sorted.
-     */
 
     // sort number one
     public static void bubbleSort(int[] data) {
-        int len = data.length - 1;
-        for (int i = 0; i < len; i++) {
-            if (data[i] < data[i + 1] && i != len - 1) {
-                i++;
-            }
-            if (data[i] > data[i + 1]) {
-                int current = data[i];
-                data[i] = data[i + 1];
-                data[i + 1] = current;
-                i = -1;
+        int placeHolder = 0;
+        int size = data.length;
+
+        // runs loops for amount of digits
+        for (int b = 0; b < size; b++) {
+            for (int c = 1; c < size - b; c++) {
+
+                // if there is a value that is greater behind
+                if (data[c - 1] > data[c]) {
+
+                    placeHolder = data[c - 1]; //store prev value temporarily
+                    data[c - 1] = data[c]; //swap
+                    data[c] = placeHolder; //swap
+                }
+
             }
         }
     }
@@ -28,6 +29,7 @@ public class Sorts {
         int start = 0;
         int temp = 0;
         int min = 0;
+
         for (int i = 0; i < ary.length; i++) {
             for (int j = start; j < ary.length; j++) {
                 min = ary[start];
@@ -38,32 +40,40 @@ public class Sorts {
                     ary[j] = temp;
                 }
             }
-            start++;
+            
+            start++; // increement the start value
         }
     }
 
     // sort number three
     public static void insertionSort(int[] data) {
-        int len = data.length;
-        for (int b = 1; b < len; b++) {
-            int index = b - 1;
-            int current = data[b];
+        int index = 0;
+        int current = 0;
+        int size = data.length;
+
+        for (int b = 1; b < size; b++) {
+            index = b - 1;
+            current = data[b];
+
+            // problem: index goes out of bound when approaches zero
+            // needs 2 conditions
+
             while ((index >= 0) && (data[index] > current)) {
                 data[index + 1] = data[index];
                 index--;
             }
+            // swaps
             data[index + 1] = current;
         }
     }
 
-
+    // my own method to compare arrays
     public static boolean compares(int[] a, int[] b) {
         int lenA = a.length;
         int lenB = b.length;
         if (lenA != lenB) {
             return false;
         }
-
         for (int i = 0; i < lenA; i++) {
             if (a[i] != b[i]) {
                 return false;
@@ -72,8 +82,27 @@ public class Sorts {
         return true;
     }
 
-  public static void main(String[] args) {
-  
-  }
 
+    public static void main(String[] args) {
+        int z = 0;
+        System.out.println("here are the random test cases");
+        while (z < 15) {
+            Random nft = new Random();
+            int[] nums = new int[300];
+            int[] bums = new int[300];
+            for (int br = 0; br < nums.length; br++) {
+                int temp = nft.nextInt();
+                nums[br] = temp;
+                bums[br] = temp;
+            }
+
+            Arrays.sort(nums);
+            selectionSort(bums);
+            System.out.println("");
+            System.out.println(compares(nums, bums));
+            System.out.println(Arrays.equals(nums, bums));
+            z++;
+        }
+
+    }
 }
