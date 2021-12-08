bbfb5a4dbcf0b871f8723cfc42b4fec47ec1e411
esohel30 <esohel30@stuy.edu>
2021-12-08 17:41:04 -0500

    finished addvertical and test a lot of cases; everything works so far

--- 09-WordSearch/WordSearch.java
+++ 09-WordSearch/WordSearch.java
@@ -1,43 +1,45 @@
 /*Lab9: Word Search generator
-*/
-public class WordSearch{
+ */
+public class WordSearch {
+    private char[][] data;
 
-    private char[][]data;
     /**Initialize the grid to the size specified
      *and fill all of the positions with '_'
      *@param rows is the starting height of the WordSearch
      *@param cols is the starting width of the WordSearch
      */
-    public WordSearch(int rows,int cols){
-      this.data = new char[rows][cols];
-      for(int i=0; i < data.length; i++){
-        for(int j=0; j <data[i].length; j++) {
-          data[i][j] = '_';
+
+    public WordSearch(int rows, int cols) {
+        this.data = new char[rows][cols];
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] = '_';
+            }
         }
-      }
     }
     /**Set all values in the WordSearch to underscores'_'*/
-    private void clear(){
-      for(int i=0; i < data.length; i++){
-        for(int j=0; j <data[i].length; j++) {
-          data[i][j] = '_';
-        }
-      }
-    }
 
+    private void clear() {
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] = '_';
+            }
+        }
+    }
     /**Each row is a new line, there is a space between each letter
      *@return a String with each character separated by spaces, and rows
      *separated by newlines.
      */
-    public String toString(){
-      String temp = "";
-      for(int i=0; i < data.length; i++){
-        for(int j=0; j < data[i].length; j++) {
-          temp += data[i][j];
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                temp += data[i][j];
+            }
+            temp += "\n";
         }
-        temp += "\n";
-      }
-      return temp;
+        return temp;
     }
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from left to right, must fit on the WordGrid, and must
@@ -50,19 +52,21 @@ public class WordSearch{
      * or there are overlapping letters that do not match, then false is returned
      * and the board is NOT modified.
      */
-    public boolean addWordHorizontal(String word,int row, int col){
-        if(data.length < col + word.length()){
-          return false;
-        }else{
-          for(int i =col; i < word.length() + col; i++){
-            data[row][i] = word.charAt(i - col);
-          }
+
+    public boolean addWordHorizontal(String word, int row, int col) {
+        if (data.length < col + word.length()) {
+            return false;
+        } else {
+            for (int i = col; i < word.length() + col; i++) {
+                if (data[row][i] != '_' && data[row][i] != word.charAt(i - col)) {
+                    return false;
+                }
+                data[row][i] = word.charAt(i - col);
+            }
         }
-      return true;
+        return true;
     }
-
-
-   /**Attempts to add a given word to the specified position of the WordGrid.
+    /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from top to bottom, must fit on the WordGrid, and must
      *have a corresponding letter to match any letters that it overlaps.
      *
@@ -73,20 +77,31 @@ public class WordSearch{
      *or there are overlapping letters that do not match, then false is returned.
      *and the board is NOT modified.
      */
-    public boolean addWordVertical(String word,int row, int col){
 
-      return true; // for now
-
-
+    public boolean addWordVertical(String word, int row, int col) {
+        if (data[0].length < row + word.length()) {
+            return false;
+        } else {
+            for (int i = row; i < word.length() + row; i++) {
+                if (data[i][col] != '_' && data[i][col] != word.charAt(i - row)) {
+                    return false;
+                }
+                data[i][col] = word.charAt(i - row);
+            }
+        }
+        return true;
     }
 
-
-
-
-
     public static void main(String[] args) {
-      WordSearch x = new WordSearch(10,10);
-      x.addWordHorizontal("bigBIrdy", 4, 2);
-      System.out.println(x);
+        WordSearch x = new WordSearch(5, 5);
+        x.addWordVertical("hello", 0, 0);
+        x.addWordHorizontal("haunt", 0, 0);
+        x.addWordHorizontal("energ", 1, 0);
+        x.addWordVertical("anber", 0, 1);
+        x.addWordHorizontal("man", 2, 2);
+
+        x.clear(); 
+
+        System.out.println(x);
     }
 }
