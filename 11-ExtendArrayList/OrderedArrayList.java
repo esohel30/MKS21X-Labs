95a0ce7369b77ba201d9aac896d4922b9f5c0983
esohel30 <esohel30@stuy.edu>
2021-12-19 21:16:58 -0500

    addded set method

--- 11-ExtendArrayList/OrderedArrayList.java
+++ 11-ExtendArrayList/OrderedArrayList.java
@@ -2,42 +2,50 @@ import java.util.*;
 import java.util.ArrayList;
 import java.io.*;
 
-public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+public class OrderedArrayList < T extends Comparable < T >> extends NoNullArrayList < T > {
 
-  public OrderedArrayList(){
-    super();
-  }
+    public OrderedArrayList() {
+        super();
+    }
 
-  public OrderedArrayList(int startingCapacity){
-    super(startingCapacity);
-  }
+    public OrderedArrayList(int startingCapacity) {
+        super(startingCapacity);
+    }
 
-  /*return the index that the value should be placed
-  when inserting into the OrderedArrayList .*/
+    /*return the index that the value should be placed
+    when inserting into the OrderedArrayList .*/
 
-  private int whereToPlace(T val){
-      int idx = super.size();
-      // takes the size of the og array list
-      for (int j = 0; j < size; j++){
-        if (val.compareTo(super.get(j)) < 0){
-          // compares the values lexicographically
-          idx = j;
-          break;
+    private int whereToPlace(T val) {
+        int idx = super.size();
+        // takes the size of the og array list
+        for (int j = 0; j < size; j++) {
+            if (val.compareTo(super.get(j)) < 0) {
+                // compares the values lexicographically
+                idx = j;
+                break;
+            }
         }
-      }
-      return idx;
-  }
-
-      @Override
-    public boolean add(T value){
-      super.add(value);
-      int where = whereToPlace(value);
-      super.remove(super.lastIndexOf(value));
-      super.add(where, value);
-      return true;
+        return idx;
     }
 
     @Override
-    public void add(int index, T value){
-      super.add(value);
+    public boolean add(T value) {
+        super.add(value);
+        int where = whereToPlace(value);
+        super.remove(super.lastIndexOf(value));
+        super.add(where, value);
+        return true;
     }
+
+    @Override
+    public void add(int idx T value) {
+        super.add(value);
+    }
+
+    @Override
+    public T set(int index, T value) {
+        T position = super.remove(index);
+        add(value);
+        return position;
+    }
+}
