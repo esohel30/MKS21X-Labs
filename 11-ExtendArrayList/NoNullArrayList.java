88ca27d954d5fedc463001cdc77c938b78462f52
esohel30 <esohel30@stuy.edu>
2021-12-19 20:45:47 -0500

    added the constructer and imported the necesasry things

---
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
+public class NoNullArrayList<T> extends ArrayList<T> {
+
+  public NoNullArrayList() {
+    super();
+  }
+
+  
