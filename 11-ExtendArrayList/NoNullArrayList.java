55ed261838e870f2974c0c6aefb9db4a99aa098c
esohel30 <esohel30@stuy.edu>
2021-12-19 20:55:45 -0500

    added 2nd add method with specified index and finxed format of rest of code

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -2,38 +2,43 @@ import java.util.ArrayList;
 import java.util.*;
 import java.io.*;
 
-public class NoNullArrayList<T> extends ArrayList<T> {
+public class NoNullArrayList < T > extends ArrayList < T > {
 
-  public NoNullArrayList() {
-    super();
-  }
-
-  public NoNullArrayList(int initialcap) {
-    super(initialcap);
-  }
-
-  @Override
-  public T set(int ind, T current) {
-    if (current == null) {
-      throw new IllegalArgumentException();
+    public NoNullArrayList() {
+        super();
     }
-    else {
-      super.set(ind, current);
-    }
-    return current;
-  }
 
-  @Override
-  public boolean add(T current) {
-    if (current == null) {
-      throw new IllegalArgumentException();
+    public NoNullArrayList(int initialcap) {
+        super(initialcap);
     }
-    else {
-      super.add(current);
+
+    @Override
+    public T set(int idx, T current) {
+        if (current == null) {
+            throw new IllegalArgumentException();
+        } else {
+            super.set(idx, current);
+        }
+        return current;
     }
-    return true;
-  }
 
+    @Override
+    public boolean add(T current) {
+        if (current == null) {
+            throw new IllegalArgumentException();
+        } else {
+            super.add(current);
+        }
+        return true;
+    }
 
+    @Override
+    public void add(int idx, T current) {
+        if (current == null) {
+            throw new IllegalArgumentException();
+        } else {
+            super.add(idx, current);
+        }
+    }
 
 }
