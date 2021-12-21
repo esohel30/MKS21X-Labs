1f371a94352071ad684590a943fe67fc5ff13b50
esohel30 <esohel30@stuy.edu>
2021-12-20 20:13:45 -0500

    added and tested several cases and everything seems to work: no null returns illegal thing when provided with null and ordered array list puts everything in order

--- 11-ExtendArrayList/tester.java
+++ 11-ExtendArrayList/tester.java
@@ -3,24 +3,43 @@ public class tester{
   public static void main(String[] args) {
     OrderedArrayList<String> list = new OrderedArrayList<String>();
 
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
-    list.add("hello");
+    list.add("z");
+    list.add("y");
+    list.add("x");
+    list.add("w");
+    list.add("v");
+    list.add("u");
+    list.add("t");
+    list.add("s");
+    list.add("r");
+    list.add("q");
+    list.add("p");
+    list.add("o");
+    list.add("n");
+    list.add("m");
+    list.add("l");
+    list.add("k");
+    list.add("j");
+    list.add("HELLICOPTER HILIOPTER");
+
+
+
+    NoNullArrayList<String> list2 = new NoNullArrayList<String>();
+
+    list2.add("no");
+    list2.add("no");
+    list2.add("no");
+    list2.add("no");
+    list2.add("no");
+    list2.add("no");
+    list2.add("no");
 
 
     System.out.print(list.toString());
+    System.out.print("\n");
+    System.out.print(list2.toString());
+
+
 
 
 
