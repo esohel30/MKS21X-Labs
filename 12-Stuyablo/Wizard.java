6c7d30adb962d0ee30092916b2cf12a314750708
esohel30 <esohel30@stuy.edu>
2022-01-17 21:01:00 -0500

    working on wizard class methods; finished the methods getmana and the constructors

--- 12-Stuyablo/Wizard.java
+++ 12-Stuyablo/Wizard.java
@@ -1,19 +1,19 @@
 public class Wizard extends Adventurer {
-    private int  rage;
+    private int  mana;
     private String warcry;
 
-    public Warrior(){
-	     this("Magnus");
+    public Wizard(){
+	     this("Albus");
     }
 
-    public Warrior(String name){
-	     this(name,"Valhalllaaaaa!!", 18);
+    public Wizard(String name){
+	     this(name,"ALAKAZAM", 18);
     }
 
-    public Warrior(String name, String warcry, int rage){
+    public Warrior(String name, String warcry, int mana){
       super(name,30+(int)(Math.random()*10));
       setWarcry(warcry);
-      setRage(rage);
+      setMana(mana);
     }
 
     //warrior methods
@@ -21,28 +21,28 @@ public class Wizard extends Adventurer {
     public void attack(Damageable other){
     	  int damage = (int)(Math.random()*10)+1;
   	    other.applyDamage(damage);
-  	    setRage(getRage() + 1);
+  	    setMana(getMana() + 1);
   	    System.out.println(this +
             " attacked " + other + " for " +
             damage + " damage!");
     }
 
     public void specialAttack(Damageable other){
-	     if(getRage() >= 10){
+	     if(getMana() >= 10){
   	        int damage = (int)(Math.random()*20)+1;
             other.applyDamage(damage);
-            System.out.println(this + " unleashes his fury upon "
+            System.out.println(this + " unleashes his wizard powers upon "
              + other + " for " + damage + " damage! "+warcry);
             setRage(getRage() - 10);
 	    }else{
-			    System.out.println("Not enough rage! ");
+			    System.out.println("Not enough mana! ");
           attack(other);
 	    }
     }
 
     //get methods
 
-    public int getRage(){
+    public int getMana(){
 	     return rage;
     }
 
